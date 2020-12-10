7ab6893bd25b0d1682adaa9c0ee8e7edfe8ceae2
Tyler Neal <TylerNeal1307@gmail.com>
2020-12-10 19:48:46 +0300

    Fixed 02_Functions 10.12.20 07:48pm

--- KotlinBasic/02_Functions/app/src/main/java/com/skillbox/a02_functions/Main.kt
+++ KotlinBasic/02_Functions/app/src/main/java/com/skillbox/a02_functions/Main.kt
@@ -3,18 +3,18 @@ package com.skillbox.a02_functions
 import kotlin.math.sqrt
 
 fun main() {
-    val solutionSum = solveEquation(a = 3, b = -14, c = -5)
-    print("Sum = $solutionSum")
+    val solutionSum = solveEquation(a = 2, b = 4, c = 7)
+    print("Sum = ${solutionSum ?: "null, because equation has negative discriminant."}")
 }
 
-fun solveEquation (a: Int, b: Int, c: Int): Double {
+fun solveEquation (a: Int, b: Int, c: Int): Double? {
     val d = (b * b - 4 * a * c).toDouble()  // calculate discriminant
     var sum = 0.0
-    if (d > 0) {
+    return if (d > 0) {
         val x1 = (-b + sqrt(d)) / (2 * a)  // 1'st root
         val x2 = (-b - sqrt(d)) / (2 * a)  // 2'st root
         println("First root = ${x1}\nSecond root = $x2")
-        sum = (x1 + x2)
-    }
-    return sum
+        x1 + x2
+    } else null
+
 }
