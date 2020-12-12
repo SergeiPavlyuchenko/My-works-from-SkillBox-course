73924a0b6fe86c68e875971e4ed3be6618b4a86c
Tyler Neal <TylerNeal1307@gmail.com>
2020-12-12 12:17:42 +0300

    Fixed 03_Loops_Recursion_NullableTypes 12.12.20 12:17 am

--- KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/a03_loops_recursion_nullabletypes/Main.kt
+++ KotlinBasic/03_Loops_Recursion_NullableTypes/app/src/main/java/com/skillbox/a03_loops_recursion_nullabletypes/Main.kt
@@ -5,31 +5,37 @@ import kotlin.math.abs
 fun main() {
 
     print("Enter the number: ")
-    val numbers = readLine()?.toIntOrNull() ?: return print("This is not a number")
-    var count = 0
+    val numbers = inputInt()
+    var positiveCount = 0
     var sum = 0
     var isDone = false
     while (!isDone) {
-        for (i in 0 until numbers) {
-            val number = readLine()?.toIntOrNull() ?: break
+        var repeatCount = 1
+        repeat(numbers) {
+            repeatCount++
+            val number = inputInt()
             if (number > 0) {
-                count++
+                positiveCount++
                 sum += number
             }
-            if (i == numbers - 1) isDone = true
+            if (repeatCount == numbers) isDone = true
         }
         if(!isDone) {
             println("Incorrect data. Enter $numbers numbers again.")
-            count = 0
+            positiveCount = 0
             sum = 0
         }
     }
-    println("The number of positive numbers = $count\nThe sum of this numbers = $sum")
+    println("The number of positive numbers = $positiveCount\nThe sum of this numbers = $sum")
     println("\nEnter a number for \"gcd\" function")
-    print("Greatest common factor = ${readLine()?.toIntOrNull()?.let { gcd(it, sum) } ?: "null, because you data is incorrect."}")
+    print("Greatest common factor = ${ gcd(inputInt(), sum) }")
 
 }
 
 tailrec fun gcd(a: Int, b: Int): Int {
     return if (b == 0) abs(a) else gcd(b, a % b)
+}
+
+tailrec fun inputInt(): Int {
+    return readLine()?.toIntOrNull() ?: inputInt()
 }
