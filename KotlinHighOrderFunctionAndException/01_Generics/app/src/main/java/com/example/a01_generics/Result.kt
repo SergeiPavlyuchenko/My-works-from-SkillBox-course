00fbfb97f4875a252c43f74adadb654ba1e36186
Sergei Pavlyuchenko <tylerneal1307@gmail.com>
2020-12-30 09:54:16 +0300

    KotlinHighOrderFunctionAndException_01_Generics progress at the 30.12.20 09:55

--- KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/example/a01_generics/Result.kt
+++ KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/example/a01_generics/Result.kt
@@ -1,19 +1,18 @@
 package com.example.a01_generics
 
-sealed class Result<out T, R> {
+sealed class Result<out T: Number, R> {
+    abstract val item: Result<Int, String>
 
-
-
-
-
-}
-
-fun returnObject(): Result<Int, String> {
-    return object : Result<Int, String>
+    fun returnObject(): Result<Int, String> {
+        return item
+    }
 }
 
 
-data class Success<T, R> (val item: T) : Result<T, R>()
 
 
-data class Error<T, R> (val item: R) : Result<T, R>()
+
+data class Success<T : Number, R> (val newItem: T, override val item: Result<Int, String>) : Result<T, R>()
+
+
+data class Error<T: Number, R> (val newItem: R, override val item: Result<Int, String>) : Result<T, R>()
