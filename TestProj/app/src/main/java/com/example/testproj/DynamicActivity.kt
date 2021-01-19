e5a3845351d10b2eb1ea6637b19f402117858892
Sergei Pavlyuchenko <tylerneal1307@gmail.com>
2021-01-19 17:50:54 +0300

    ViewAndLayout progress at 19.01.21 17:50

--- TestProj/app/src/main/java/com/example/testproj/DynamicActivity.kt
+++ TestProj/app/src/main/java/com/example/testproj/DynamicActivity.kt
@@ -3,6 +3,7 @@ package com.example.testproj
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,47 +14,40 @@ import kotlin.random.Random
 
 class DynamicActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityDinamicBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDinamicBinding.inflate(layoutInflater)
-        val view = R.layout.activity_dinamic
-        setContentView(view)
+        val binding = ActivityDinamicBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val container = binding.container
 
 
         binding.addButton.setOnClickListener {
             val textToAdd = binding.textInput.text.toString()
-
-            //Не работает код ниже, ни закомментированный, ни открытыый.
-            //Хотя он полностью повторяет код из урока
-
-
-
-            val viewFromItemText = findViewById<LinearLayout>(R.id.itemTextContainer)
-            viewFromItemText.apply {
-                this.textView
+            val textViewToAdd = TextView(this).apply {
+            text = textToAdd
+            layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                gravity = when (Random.nextInt(3)) {
+                    0 -> Gravity.CENTER
+                    1 -> Gravity.END
+                    else -> Gravity.START
+                }
             }
-
-
-//            val textViewToAdd = TextView(this).apply {
-//                text = textToAdd
-//                layoutParams = LinearLayout.LayoutParams(
-//                    LinearLayout.LayoutParams.WRAP_CONTENT,
-//                    LinearLayout.LayoutParams.WRAP_CONTENT
-//                ).apply {
-//                    gravity = when (Random.nextInt(3)) {
-//                        0 -> Gravity.CENTER
-//                        1 -> Gravity.END
-//                        else -> Gravity.START
-//                    }
-//                }
-//            }
-            val viewToAdd = layoutInflater.inflate(R.layout.item_text, binding.container, false)
+        }
+            container.addView(textViewToAdd)
+            /*val viewToAdd = layoutInflater.inflate(R.layout.item_text, container, false)
             viewToAdd.apply {
-
+                this.findViewById<TextView>(R.id.textView).text = textToAdd
+                findViewById<Button>(R.id.deleteButton).setOnClickListener {
+                    container.removeView(this)
+                }
             }
+            container.addView(viewToAdd)*/
         }
 
 
