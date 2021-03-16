59d12127ecb9c1a980a83784547fb3bb44a1aba4
Sergei Pavlyuchenko <TylerNeal1307@gmail.com>
2021-03-16 13:20:35 +0300

    HomeWork 16.8 progress at the 16.03.21 13:20

--- Lists_1/app/src/main/java/com/skillbox/lists_1/InputDataDialog.kt
+++ Lists_1/app/src/main/java/com/skillbox/lists_1/InputDataDialog.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Spinner
@@ -16,14 +17,13 @@ import kotlin.properties.Delegates
 
 class InputDataDialog : DialogFragment() {
 
-    private val binding by viewBinding(DialogInputDataBinding::bind)
-
     private val fragment
         get() = parentFragment
 
     private val dialogInterfaceListener: DialogInterfaceListener?
         get() = fragment?.let { it as? DialogInterfaceListener }
 
+    private lateinit var dialogViews: View
     private lateinit var nameEditText: EditText
     private lateinit var avatarLinkEditText: EditText
     private lateinit var genreSpinner: Spinner
@@ -33,7 +33,7 @@ class InputDataDialog : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val dialogViews = requireActivity().layoutInflater.inflate(R.layout.dialog_input_data, null)
+        dialogViews = requireActivity().layoutInflater.inflate(R.layout.dialog_input_data, null)
         nameEditText = dialogViews.findViewById(R.id.gameNameEditText)
         avatarLinkEditText = dialogViews.findViewById(R.id.avatarLinkEditText)
         genreSpinner = dialogViews.findViewById(R.id.gameGenresSpinner)
@@ -42,25 +42,20 @@ class InputDataDialog : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val name = nameEditText.text.toString()
-        val avatarLink = avatarLinkEditText.text.toString()
-        val genre = genreSpinner.selectedItem.toString()
-        val rate = rateSpinner.selectedItem.toString().toFloat()
-        val isCoop = isCoopCheckBox.isChecked
-        return dialogLaunch(name, avatarLink, genre, rate, isCoop)
+
+        return dialogLaunch(/*name, avatarLink, genre, rate, isCoop*/)
     }
 
-    private fun dialogLaunch(
-        name: String,
-        avatarLink: String,
-        genre: String,
-        rate: Float,
-        isCoop: Boolean
-    ): Dialog {
+    private fun dialogLaunch(): Dialog {
         return AlertDialog.Builder(requireContext())
             .setTitle("            ${getString(R.string.text_enter_game_information)}")
-            .setView(R.layout.dialog_input_data)
+            .setView(dialogViews)
             .setPositiveButton("Add") { _: DialogInterface, _: Int ->
+                val name = nameEditText.text.toString()
+                val avatarLink = avatarLinkEditText.text.toString()
+                val genre = genreSpinner.selectedItem.toString()
+                val rate = rateSpinner.selectedItem.toString().toFloat()
+                val isCoop = isCoopCheckBox.isChecked
                 val game: GameGenre = when (genre) {
                     getString(R.string.text_shooter) -> GameGenre.Shooters(
                         name,
