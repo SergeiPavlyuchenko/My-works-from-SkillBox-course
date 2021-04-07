7d5da1946a3859bd46fc8350471893fd670b5921
Sergei Pavlyuchenko <tylerneal1307@gmail.com>
2021-04-07 11:25:30 +0300

    HomeWork 17.12 07.04.21 11:25

--- Lists_2/app/src/main/java/com/skillbox/lists_2/HostFragment.kt
+++ Lists_2/app/src/main/java/com/skillbox/lists_2/HostFragment.kt
@@ -6,28 +6,14 @@ import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.skillbox.lists_2.databinding.FragmentHostBinding
 
-class HostFragment: Fragment(R.layout.fragment_host) {
+class HostFragment : Fragment(R.layout.fragment_host) {
 
     private val binding by viewBinding(FragmentHostBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        /*if (savedInstanceState == null) {
+        if (savedInstanceState == null) {
             childFragmentManager.transaction(R.id.containerHostFragment, MainFragment(), true)
-        }*/
-
-        binding.test1TransactionRecyclerViewButton.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.containerActivity, TestFragment(), "TestFragment")
-                .addToBackStack(null)
-                .commit()
-        }
-
-        binding.test2TransactionRecyclerViewButton.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.containerActivity, TestFragment(), "TestFragment")
-                .addToBackStack(null)
-                .commit()
         }
     }
 
--- Lists_2/app/src/main/java/com/skillbox/lists_2/TestFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.skillbox.lists_2
-
-
-import androidx.fragment.app.Fragment
-
-
-class TestFragment: Fragment(R.layout.fragment_test_transaction) {
-
-}
